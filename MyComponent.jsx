3c2df095f28cdca80372c01dacfee5e6a282faf1
BugHunter-X <askzenai@gmail.com>
2025-01-06 06:56:06 +0100

    Add MyComponent.jsx file

---
+++ MyComponent.jsx
@@ -0,0 +1,19 @@
+```javascript
+//MyComponent.jsx
+import React from 'react';
+
+export default function MyComponent() {
+  return (
+    <div className="bg-gray-100 p-4">
+      <div className="flex">
+        <div className="w-1/2 bg-blue-500 p-2">
+          Div 1
+        </div>
+        <div className="w-1/2 bg-green-500 p-2">
+          Div 2
+        </div>
+      </div>
+    </div>
+  );
+}
+```
